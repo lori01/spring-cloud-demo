eee6f66560ed1782c436362d379ab906db7b98f6
lori01 <sephy9527@qq.com>
2019-04-02 19:31:42 +0800

    page 修改

--- api-server-mybatis/src/main/java/com/daimeng/api/ApiServerApplication.java
+++ api-server-mybatis/src/main/java/com/daimeng/api/ApiServerApplication.java
@@ -18,54 +18,7 @@ import com.github.pagehelper.PageInterceptor;
 @MapperScan("com.daimeng.**.mapper")
 public class ApiServerApplication {
 
-	@Value("${datasource.dialect}")
-	private String dialect;
-	@Value("${pagehelper.offsetAsPageNum}")
-	private String offsetAsPageNum;
-	@Value("${pagehelper.rowBoundsWithCount}")
-	private String rowBoundsWithCount;
-	@Value("${pagehelper.reasonable}")
-	private String reasonable;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiServerApplication.class, args);
 	}
-
-	/**
-	 * mysql 分页插件
-	 * @return
-	 */
-	@Bean
-	public PageHelper pageHelper() {
-		PageHelper pageHelper = new PageHelper();
-		Properties properties = new Properties();
-		properties.setProperty("offsetAsPageNum", offsetAsPageNum);
-		properties.setProperty("rowBoundsWithCount", rowBoundsWithCount);
-		properties.setProperty("reasonable", reasonable);
-		System.out.println("######################pageHelper#########################");
-		System.out.println(dialect);
-		System.out.println(offsetAsPageNum);
-		System.out.println(rowBoundsWithCount);
-		System.out.println(reasonable);
-		System.out.println("######################pageHelper#########################");
-		properties.setProperty("dialect", dialect); // 配置mysql数据库的方言
-		pageHelper.setProperties(properties);
-		return pageHelper;
-	}
-	/**
-	 * mysql 分页插件
-	 * @return
-	 */
-	@Bean
-	public PageInterceptor pageInterceptor() {
-		PageInterceptor pageInterceptor = new PageInterceptor();
-		Properties properties = new Properties();
-		System.out.println("######################pageInterceptor#########################");
-		System.out.println(dialect);
-		System.out.println("######################pageInterceptor#########################");
-		properties.setProperty("helperDialect", dialect); // 配置mysql数据库的方言
-		pageInterceptor.setProperties(properties);
-		return pageInterceptor;
-	}
-
 }
