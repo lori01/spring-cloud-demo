9faea6e96210eadf763d3e919c2b99e75128f746
lori01 <sephy9527@qq.com>
2019-04-27 17:59:54 +0800

    各种echarts图搬运

--- web-thymeleaf/src/main/java/com/daimeng/web/chart/action/ChartController.java
+++ web-thymeleaf/src/main/java/com/daimeng/web/chart/action/ChartController.java
@@ -12,8 +12,39 @@ import com.daimeng.web.common.BaseController;
 @Controller
 @RequestMapping("/chart")
 public class ChartController extends BaseController {
-	@RequestMapping("/index")
-	public String index(Model model) {
-		return "chart/index";
+	//折线图
+	@RequestMapping("/line")
+	public String line(Model model) {
+		return "chart/line";
+	}
+	//柱状图
+	@RequestMapping("/bar")
+	public String bar(Model model) {
+		return "chart/bar";
+	}
+	//饼图
+	@RequestMapping("/pie")
+	public String pie(Model model) {
+		return "chart/pie";
+	}
+	//雷达图
+	@RequestMapping("/radar")
+	public String radar(Model model) {
+		return "chart/radar";
+	}
+	//关系图
+	@RequestMapping("/graph")
+	public String graph(Model model) {
+		return "chart/graph";
+	}
+	//仪表盘图
+	@RequestMapping("/gauge")
+	public String gauge(Model model) {
+		return "chart/gauge";
+	}
+	//漏斗图
+	@RequestMapping("/funnel")
+	public String funnel(Model model) {
+		return "chart/funnel";
 	}
 }
