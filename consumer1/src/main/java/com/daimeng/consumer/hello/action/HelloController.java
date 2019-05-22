80f9d5d966e2aed2952d16b9867fcc755e6a173d
lori01 <sephy9527@qq.com>
2019-05-22 16:27:23 +0800

    config client

--- consumer1/src/main/java/com/daimeng/consumer/hello/action/HelloController.java
+++ consumer1/src/main/java/com/daimeng/consumer/hello/action/HelloController.java
@@ -1,6 +1,7 @@
 package com.daimeng.consumer.hello.action;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,21 @@ import com.daimeng.consumer.hello.remote.HelloRemote;
 @RestController
 public class HelloController {
 
+	@Value("${url.info}")
+    private String urlInfo;
+	@Value("${perperties.type}")
+	private String type;
+	@Value("${perperties.id}")
+	private String id;
+	@Value("${url}")
+	private String url;
+	
 	@Autowired
     HelloRemote helloRemote;
 	
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
-        return helloRemote.hello(name + " from consumer 1");
+        return helloRemote.hello(name + " from consumer 1;" + url);
     }
     
 }
