9d8a0ee85e55885d01ee98abe71c429b66ad93bd
lori01 <sephy9527@qq.com>
2019-08-02 17:04:39 +0800

    xss

--- web-thymeleaf/src/main/java/com/daimeng/filter/XssHttpServletRequestWrapper.java
+++ web-thymeleaf/src/main/java/com/daimeng/filter/XssHttpServletRequestWrapper.java
@@ -1,101 +1,264 @@
 package com.daimeng.filter;
 
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
 import org.apache.commons.lang.StringUtils;
+import org.owasp.esapi.ESAPI;
 
 import com.daimeng.util.JsoupUtil;
 
 public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper {
 
-	HttpServletRequest orgRequest = null;  
-    private boolean isIncludeRichText = false;
-    
-	public XssHttpServletRequestWrapper(HttpServletRequest request, boolean isIncludeRichText) {
-		super(request);  
-        orgRequest = request;
-        this.isIncludeRichText = isIncludeRichText;
+	HttpServletRequest orgRequest = null;
+	private boolean isIncludeRichText = false;
+
+	public XssHttpServletRequestWrapper(HttpServletRequest request,
+			boolean isIncludeRichText) {
+		super(request);
+		orgRequest = request;
+		this.isIncludeRichText = isIncludeRichText;
 	}
-	
-	/** 
-	    * 覆盖getParameter方法，将参数名和参数值都做xss过滤。<br/> 
-	    * 如果需要获得原始的值，则通过super.getParameterValues(name)来获取<br/> 
-	    * getParameterNames,getParameterValues和getParameterMap也可能需要覆盖 
-	    */  
-	    @Override  
-	    public String getParameter(String name) {
-	        Boolean flag = ("context".equals(name) || name.endsWith("WithHtml"));
-	        if( flag && !isIncludeRichText){
-	            return super.getParameter(name);
-	        }
-	        name = JsoupUtil.clean(name,true);
-	        String value = super.getParameter(name);  
-	        if (StringUtils.isNotBlank(value)) {
-	            value = JsoupUtil.clean(value,false);
-	        }
-	        return value;  
-	    }  
-	    
-	    /**
-	     * 
-	    * @see javax.servlet.ServletRequestWrapper#getParameterValues(java.lang.String)
-	    * @方法名称: getParameterValues 
-	    * @路径 com.daimeng.filter 
-	    * @功能描述: post方法 
-	    * @作者 daimeng@tansun.com.cn
-	    * @创建时间 2019年4月8日 下午5:37:32 
-	    * @version V1.0   
-	    * @param name
-	    * @return
-	     */
-	    @Override
-	    public String[] getParameterValues(String name) {
-	    	String[] arr = super.getParameterValues(name);
-	    	if(arr != null){
-	    		for (int i=0;i<arr.length;i++) {
-	    			arr[i] = JsoupUtil.clean(arr[i],false);
-	    		}
-	    	}
-	    	return arr;
-	    }
-	    
-	  
-	    /** 
-	    * 覆盖getHeader方法，将参数名和参数值都做xss过滤。<br/> 
-	    * 如果需要获得原始的值，则通过super.getHeaders(name)来获取<br/> 
-	    * getHeaderNames 也可能需要覆盖 
-	    */  
-	    @Override  
-	    public String getHeader(String name) {  
-	        name = JsoupUtil.clean(name,true);
-	        String value = super.getHeader(name);  
-	        if (StringUtils.isNotBlank(value)) {  
-	            value = JsoupUtil.clean(value,true); 
-	        }  
-	        return value;  
-	    }  
-	  
-	    /** 
-	    * 获取最原始的request 
-	    * 
-	    * @return 
-	    */  
-	    public HttpServletRequest getOrgRequest() {  
-	        return orgRequest;  
-	    }  
-	  
-	    /** 
-	    * 获取最原始的request的静态方法 
-	    * 
-	    * @return 
-	    */  
-	    public static HttpServletRequest getOrgRequest(HttpServletRequest req) {  
-	        if (req instanceof XssHttpServletRequestWrapper) {  
-	            return ((XssHttpServletRequestWrapper) req).getOrgRequest();  
-	        }  
-	  
-	        return req;  
-	    }  
+
+	/**
+	 * 覆盖getParameter方法，将参数名和参数值都做xss过滤。<br/>
+	 * 如果需要获得原始的值，则通过super.getParameterValues(name)来获取<br/>
+	 * getParameterNames,getParameterValues和getParameterMap也可能需要覆盖
+	 */
+	@Override
+	public String getParameter(String name) {
+		Boolean flag = ("context".equals(name) || name.endsWith("WithHtml"));
+		if (flag && !isIncludeRichText) {
+			return super.getParameter(name);
+		}
+		name = JsoupUtil.clean(name, true);
+		String value = super.getParameter(name);
+		if (StringUtils.isNotBlank(value)) {
+			value = JsoupUtil.clean(value, false);
+		}
+		return value;
+	}
+
+	/**
+	 * 
+	 * @see javax.servlet.ServletRequestWrapper#getParameterValues(java.lang.String)
+	 * @方法名称: getParameterValues
+	 * @路径 com.daimeng.filter
+	 * @功能描述: post方法
+	 * @作者 daimeng@tansun.com.cn
+	 * @创建时间 2019年4月8日 下午5:37:32
+	 * @version V1.0
+	 * @param name
+	 * @return
+	 */
+	@Override
+	public String[] getParameterValues(String name) {
+		String[] arr = super.getParameterValues(name);
+		if (arr != null) {
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = JsoupUtil.clean(arr[i], false);
+			}
+		}
+		return arr;
+	}
+
+	/**
+	 * 覆盖getHeader方法，将参数名和参数值都做xss过滤。<br/>
+	 * 如果需要获得原始的值，则通过super.getHeaders(name)来获取<br/>
+	 * getHeaderNames 也可能需要覆盖
+	 */
+	@Override
+	public String getHeader(String name) {
+		name = JsoupUtil.clean(name, true);
+		String value = super.getHeader(name);
+		if (StringUtils.isNotBlank(value)) {
+			value = JsoupUtil.clean(value, true);
+		}
+		return value;
+	}
+
+	/**
+	 * 获取最原始的request
+	 * 
+	 * @return
+	 */
+	public HttpServletRequest getOrgRequest() {
+		return orgRequest;
+	}
+
+	/**
+	 * 获取最原始的request的静态方法
+	 * 
+	 * @return
+	 */
+	public static HttpServletRequest getOrgRequest(HttpServletRequest req) {
+		if (req instanceof XssHttpServletRequestWrapper) {
+			return ((XssHttpServletRequestWrapper) req).getOrgRequest();
+		}
+
+		return req;
+	}
+
+	public static void main(String[] args) {
+		String value = "<script>function a=function(){}</script>";
+		System.out.println(cleanXSS(value));
+	}
+
+	private static String cleanXSS(String value) {
+		if (value != null) {
+			// 推荐使用ESAPI库来避免脚本攻击
+			// value = ESAPI.encoder().canonicalize(value);
+			value = encodeHtml(value);
+			// 避免空字符串
+			value = value.trim();
+
+			// 避免script 标签
+			Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免src形式的表达式
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 删除单个的 </script> 标签
+			scriptPattern = Pattern.compile("</script>",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 删除单个的<script ...> 标签
+			scriptPattern = Pattern.compile("<script(.*?)>",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 eval(...) 形式表达式
+			scriptPattern = Pattern.compile("eval\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 e­xpression(...) 表达式
+			scriptPattern = Pattern.compile("expression\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 javascript: 表达式
+			scriptPattern = Pattern.compile("javascript:",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 vbscript: 表达式
+			scriptPattern = Pattern.compile("vbscript:",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 οnlοad= 表达式
+			scriptPattern = Pattern.compile("onload(.*?)=",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+			// 避免 onXX= 表达式
+			scriptPattern = Pattern.compile("on.*(.*?)=",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+
+		}
+		return value;
+	}
+
+	public static String stripXSSAndSql(String value) {
+		if (value != null) {
+			// NOTE: It's highly recommended to use the ESAPI library and
+			// uncomment the following line to
+			// avoid encoded attacks.
+			// value = ESAPI.encoder().canonicalize(value);
+			// Avoid null characters
+			/** value = value.replaceAll("", ""); ***/
+			// Avoid anything between script tags
+			Pattern scriptPattern = Pattern
+					.compile(
+							"<[\r\n| | ]*script[\r\n| | ]*>(.*?)</[\r\n| | ]*script[\r\n| | ]*>",
+							Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid anything in a
+			// src="http://www.yihaomen.com/article/java/..." type of
+			// e-xpression
+			scriptPattern = Pattern.compile(
+					"src[\r\n| | ]*=[\r\n| | ]*[\\\"|\\\'](.*?)[\\\"|\\\']",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Remove any lonesome </script> tag
+			scriptPattern = Pattern.compile("</[\r\n| | ]*script[\r\n| | ]*>",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Remove any lonesome <script ...> tag
+			scriptPattern = Pattern.compile("<[\r\n| | ]*script(.*?)>",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid eval(...) expressions
+			scriptPattern = Pattern.compile("eval\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid e-xpression(...) expressions
+			scriptPattern = Pattern.compile("e-xpression\\((.*?)\\)",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid javascript:... expressions
+			scriptPattern = Pattern.compile(
+					"javascript[\r\n| | ]*:[\r\n| | ]*",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid vbscript:... expressions
+			scriptPattern = Pattern.compile("vbscript[\r\n| | ]*:[\r\n| | ]*",
+					Pattern.CASE_INSENSITIVE);
+			value = scriptPattern.matcher(value).replaceAll("");
+			// Avoid onload= expressions
+			scriptPattern = Pattern.compile("onload(.*?)=",
+					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE
+							| Pattern.DOTALL);
+			value = scriptPattern.matcher(value).replaceAll("");
+		}
+		return value;
+	}
+
+	public static String encodeHtml(String input) {
+		StringBuffer out = new StringBuffer();
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+			if (c == '<') {
+				out.append("&lt;");
+			} else if (c == '>') {
+				out.append("&gt;");
+			} else if (c == '\"') {
+				out.append("&quot;");
+			} else if (c == '\'') {
+				out.append("&quot;");
+			} else if (c == '&') {
+				out.append("&amp;");
+			} else if (c > 0x20 && c < 0x126) {
+				out.append(c);
+			} else {
+				out.append("&#" + (int) c + ";");
+			}
+		}
+		return out.toString();
+	}
 
 }
