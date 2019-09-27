c0a13387412bc10ef257a93137e49e0c72de8bef
lori01 <sephy9527@qq.com>
2019-09-27 20:22:36 +0800

    敏感词测试

--- web-thymeleaf/src/main/java/com/daimeng/util/BadWordUtils.java
+++ web-thymeleaf/src/main/java/com/daimeng/util/BadWordUtils.java
@@ -246,9 +246,9 @@ public class BadWordUtils {
 		Map<String,String> map = BadWordUtils.wordMap;
 		
 		System.out.println("敏感词的数量：" + BadWordUtils.wordMap.size());
-		String string = "太多的伤感情怀也许只局限于饲养基地 荧幕中的情节基地，主人公尝试着去用某种方式渐渐的很潇洒地释自杀指南怀那些自己经历的伤感。"
-						+ "然后法轮功 我们的扮演的角色就是跟随着主人公的喜红客联盟 怒哀乐而过于牵强的把自己的情感也附加于银幕情节中，然后感动就流泪，"
-						+ "难过就躺在某一个人的怀里尽情的阐述心扉或者手机卡复制器一个人一杯红酒一部电影在夜三级片 深人静的晚上，关上电话静静的发呆着。";
+		String string = "这就是人生，这里是高楼大厦，这里是高楼，也是大厦。可能是人力，可能是资源。"
+						+ "基地的人生，基础的人生，都是人生。"
+						+ "福州福建赋值肤质。";
 		System.out.println("待检测语句字数：" + string.length());
 		long beginTime = System.currentTimeMillis();
 		Set<String> set = BadWordUtils.getBadWord(string, 2);
