9dd370f1218360f1da793d8cd665b9282156559f
lori01 <sephy9527@qq.com>
2019-11-14 21:47:16 +0800

    spring监控

--- web-thymeleaf/src/main/java/com/daimeng/WebServerApplication.java
+++ web-thymeleaf/src/main/java/com/daimeng/WebServerApplication.java
@@ -1,23 +1,15 @@
 package com.daimeng;
 
-import java.util.Properties;
-
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInterceptor;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.daimeng.**.mapper")
+//@ImportResource(locations = { "classpath:druid-bean.xml" })
 public class WebServerApplication  {
 
 	public static void main(String[] args) {
